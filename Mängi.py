daf2e0fdb8f7ae3b504ed12ad2cd6e2c4a0b0d20
KM100 <kaarelmaesalu09@gmail.com>
2018-01-01 13:37:12 +0200

    Lisasin mäng.py-se numbri arvamise lisa, aga viga on sees

--- Mängi.py
+++ Mängi.py
@@ -9,6 +9,103 @@ aken.title('Logi sisse.')
 aken.geometry('400x300+600+300')
 aken.configure(background = '#42cef4')
 
+def kontroll1():
+    id2 = id.get()
+    salakood2 = salakood.get()
+    fail = open('jäätiskoer', 'r', encoding = 'UTF-8')
+    kasutajad = []
+    for rida in fail:
+        kasutajad.append(rida.strip('\n'))
+    fail.close()
+    for i in range(int(len(kasutajad)/4)):
+        if kasutajad[i*4] == id2:
+            nr = i*4
+            kas = 'jah'
+            break
+        else:
+            kas = 'ei'
+    if kas == 'jah':
+        if salakood2 == kasutajad[nr+2]:
+            aken2 = Tk()
+            aken2.title(kasutajad[nr+1].title())
+            aken2.geometry('400x300+600+300')
+            aken2.configure(background = '#42cef4')
+            ttk.Style().configure('BC.TLabel', background = '#42cef4', foreground = 'green')
+            ttk.Style().configure('BC.TButton', background = '#f45f41', foreground = 'green')
+            raam = ttk.Frame(aken2).grid(row=0, column=0)
+            l4 = ttk.Label(raam, text = 'KASUTAJA', style = 'BC.TLabel').grid(row = 0, column = 1, pady = 10, padx = 40)
+            l5 = ttk.Label(raam, text = 'Konto omanik: ', style = 'BC.TLabel').grid(row = 1, column = 0, pady = 20, padx = 10)
+            l6 = ttk.Label(raam, text = kasutajad[nr+1].title(), style = 'BC.TLabel', foreground = 'red').grid(row=1, column = 1, sticky = W, padx = 10)
+            l7 = ttk.Label(raam, text = 'Kontol raha: ', style = 'BC.TLabel').grid(row = 2, column = 0, padx = 10)
+            l8 = ttk.Label(raam, text = kasutajad[nr+3], style = 'BC.TLabel', foreground = 'red').grid(row = 2, column = 1, sticky = W)
+            def välju():
+                aken2.destroy()
+            b2 = ttk.Button(raam, text = 'VÄLJU', style = 'BC.TButton', command = välju).grid(row = 3, column = 0, pady = 60)
+            def mängi():
+                aken2.destroy()
+                aken3 = Tk()
+                aken3.title('Mängud')
+                aken3.geometry('400x300+600+300')
+                aken3.configure(background = '#42cef4')
+                ttk.Style().configure('DE.TLabel', background = '#42cef4', foreground = 'green')
+                ttk.Style().configure('DE.TButton', background = '#f45f41', foreground = 'green')
+                l11 = ttk.Label(raam, text = 'Mängud', font = ('arial', 18, 'bold'), foreground = 'black', style = 'DE.TLabel').grid(row = 0, column = 1, pady = 20, padx = 20)
+                def arva():
+                    s = 0
+                    aken3.destroy()
+                    number = randint(1, 100)
+                    aken4 = Tk()
+                    aken4.title('Arva Number')
+                    aken4.geometry('400x300+600+300')
+                    aken4.configure(background = '#42cef4')
+                    ttk.Style().configure('EF.TLabel', background = '#42cef4', foreground = 'green')
+                    ttk.Style().configure('EF.TButton', background = '#f45f41', foreground = 'green')
+                    raam = ttk.Frame(aken4).grid(row = 0, column = 0)
+                    l12 = ttk.Label(raam, text = 'ARVA', font = ('arial', 18, 'bold'), style = 'EF.TLabel').grid(row = 0, column = 1, pady = 30)
+                    l13 = ttk.Label(raam, text = 'Sisestage arv 1-100', style = 'EF.TLabel').grid(row = 1, column = 0, pady = 20, padx = 20)
+                    e3 = ttk.Entry(raam, width = 10, textvariable = s).grid(row = 1, column = 1)
+                    def nr():
+                        if int(s) < number:
+                            l13 = ttk.Label(raam, text = 'ARV ON SUUREM', style = 'EF.TLabel', foreground = 'red', font = ('arial', 13, 'bold')).grid(row = 4, column = 0, pady = 20, padx = 40)
+                        elif int(s) > number:
+                            l13 = ttk.Label(raam, text = 'ARV ON VÄIKSEM', style = 'EF.TLabel', foreground = 'red', font = ('arial', 13, 'bold')).grid(row = 4, column = 0, pady = 20, padx = 40)
+                        else:
+                            kasutajad[nr+3]+=s
+                            fail = open('jäätiskoer', 'w', encoding = 'UTF-8')
+                            for i in range(len(kasutajad)):
+                                fail.write(str(kasutajad[i]))
+                                fail.write('\n')
+                            fail.close()
+                            kontroll1()
+                    ok_nupp = ttk.Button(raam, text = 'OK', style = 'EF.TButton', command = nr).grid(row = 2, column = 1, pady = 20, padx = 40)
+                    aken4.mainloop()
+                b4 = ttk.Button(raam, text = 'Arva number', style = 'DE.TButton', command = arva).grid(row = 1, column = 0, pady = 20, padx = 20)
+            b3 = ttk.Button(raam, text = 'MÄNGIMA', style = 'BC.TButton', command = mängi).grid(row = 3, column = 1, pady = 60)
+            aken2.mainloop()
+        else:
+            aken2 = Tk()
+            aken2.title('ERROR')
+            aken2.geometry('400x300+600+300')
+            aken2.configure(background = '#42cef4')
+            ttk.Style().configure('BC.TLabel', background = '#42cef4', foreground = 'green')
+            ttk.Style().configure('BC.TButton', background = '#f45f41', foreground = 'green')
+            raam = ttk.Frame(aken2).grid(row=0, column=0)
+            l4 = ttk.Label(raam, font = ('arial', 18, 'bold'), style = 'BC.TLabel', foreground = 'red', text = 'ERROR').grid(row = 0, column = 0, pady = 20)
+            l5 = ttk.Label(raam, font = ('arial', 13, 'bold'), style = 'BC.TLabel', foreground = 'red', text = 'VALE SALAKOOD').grid(row = 1, column = 0, pady = 40, padx = 120)
+            def välju():
+                aken2.destroy()
+            b2 = ttk.Button(raam, text = 'VÄLJU', style = 'BC.TButton', command = välju).grid(row = 2, column = 1, pady = 60, sticky = E)
+            aken2.mainloop()
+    elif kas == 'ei':
+        aken2 = Tk()
+        aken2.title('ERROR')
+        aken2.geometry('400x300+600+300')
+        aken2.configure(background = '#42cef4')
+        ttk.Style().configure('BC.TLabel', background = '#42cef4', foreground = 'green')
+        raam = ttk.Frame(aken2).grid(row=0, column=0)
+        l4 = ttk.Label(raam, font = ('arial', 18, 'bold'), style = 'BC.TLabel', foreground = 'red', text = 'ERROR').grid(row = 0, column = 0, pady = 20)
+        l5 = ttk.Label(raam, font = ('arial', 13, 'bold'), style = 'BC.TLabel', foreground = 'red', text = 'KASUTAJAT EI TUVASTATUD').grid(row = 1, column = 0, pady = 40, padx = 70)
+        aken2.mainloop()
 
 def kontroll():
     aken.destroy()
@@ -53,22 +150,32 @@ def kontroll():
                 ttk.Style().configure('DE.TButton', background = '#f45f41', foreground = 'green')
                 l11 = ttk.Label(raam, text = 'Mängud', font = ('arial', 18, 'bold'), foreground = 'black', style = 'DE.TLabel').grid(row = 0, column = 1, pady = 20, padx = 20)
                 def arva():
-                    s = StringVar()
+                    s = 0
                     aken3.destroy()
                     number = randint(1, 100)
                     aken4 = Tk()
                     aken4.title('Arva Number')
                     aken4.geometry('400x300+600+300')
                     aken4.configure(background = '#42cef4')
-                    ttk.Style().configure('EF.TLabel', background = '#42cef4', forground = 'green')
-                    ttk.Style().configure('EF.TButton', background = '#f45f41', forground = 'green')
+                    ttk.Style().configure('EF.TLabel', background = '#42cef4', foreground = 'green')
+                    ttk.Style().configure('EF.TButton', background = '#f45f41', foreground = 'green')
                     raam = ttk.Frame(aken4).grid(row = 0, column = 0)
                     l12 = ttk.Label(raam, text = 'ARVA', font = ('arial', 18, 'bold'), style = 'EF.TLabel').grid(row = 0, column = 1, pady = 30)
                     l13 = ttk.Label(raam, text = 'Sisestage arv 1-100', style = 'EF.TLabel').grid(row = 1, column = 0, pady = 20, padx = 20)
                     e3 = ttk.Entry(raam, width = 10, textvariable = s).grid(row = 1, column = 1)
                     def nr():
                         if int(s) < number:
-                            l13 = ttk.Label(raam, text = 'ARV ON SUUREM', style = 'EF.TLabel', forground = 'green', font = ('arial', 10, 'bold')).grid(row = 4, column = 0, pady = 20, padx = 40)
+                            l13 = ttk.Label(raam, text = 'ARV ON SUUREM', style = 'EF.TLabel', foreground = 'red', font = ('arial', 13, 'bold')).grid(row = 4, column = 0, pady = 20, padx = 40)
+                        elif s > number:
+                            l13 = ttk.Label(raam, text = 'ARV ON VÄIKSEM', style = 'EF.TLabel', foreground = 'red', font = ('arial', 13, 'bold')).grid(row = 4, column = 0, pady = 20, padx = 40)
+                        else:
+                            kasutajad[nr+3]+=s
+                            fail = open('jäätiskoer', 'w', encoding = 'UTF-8')
+                            for i in range(len(kasutajad)):
+                                fail.write(str(kasutajad[i]))
+                                fail.write('\n')
+                            fail.close()
+                            kontroll1()
                     ok_nupp = ttk.Button(raam, text = 'OK', style = 'EF.TButton', command = nr).grid(row = 2, column = 1, pady = 20, padx = 40)
                 b4 = ttk.Button(raam, text = 'Arva number', style = 'DE.TButton', command = arva).grid(row = 1, column = 0, pady = 20, padx = 20)
             b3 = ttk.Button(raam, text = 'MÄNGIMA', style = 'BC.TButton', command = mängi).grid(row = 3, column = 1, pady = 60)
--- test.py
+++
@@ -1,9 +0,0 @@
-from tkinter import colorchooser
-from tkinter import *
-from tkinter import ttk
-aken = Tk()
-riikv = StringVar()
-riik = ttk.Combobox(aken, textvariable=riikv)
-riik['values'] = ('USA', 'Kanada', 'Austraalia')
-riik.pack()
-aken.mainloop()
